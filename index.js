242311a279f576d2d2c43ee9c70e721442463c2b
Rafiul Islam <avoidcloud@gmail.com>
2021-06-22 22:25:40 +0600

    removed error alert

--- index.js
+++ index.js
@@ -35,9 +35,7 @@ function playSound() {
       .then((_) => {
         // Autoplay started!
       })
-      .catch((error) => {
-        alert("error");
-      });
+      .catch((error) => {});
   }
 }
 
